2031f232cbb40a88bf1a590b20390bfe02a00d17
YuSun2025-hub <ysun0295@student.monash.edu>
2026-07-14 19:42:52 +0800

    fix: restore accidentally deleted Bootstrap files

--- ysun-library/src/components/Form.vue
+++ ysun-library/src/components/Form.vue
@@ -70,7 +70,23 @@
     <Column field="isAustralian" header="Australian Resident" />
     <Column field="gender" header="Gender" />
     <Column field="reason" header="Reason" />
-</DataTable>
+  </DataTable>
+  <div class="row mt-5" v-if="submittedCards.length">
+   <div class="d-flex flex-wrap justify-content-start">
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+          <div class="card-header">
+            User Information
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+          </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
