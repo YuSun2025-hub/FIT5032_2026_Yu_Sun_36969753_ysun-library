5126bb97d637a24d67427620bdf5cb083414e190
YuSun2025-hub <ysun0295@student.monash.edu>
2026-07-14 15:58:43 +0800

    feat: complete Week 4 requirements development

--- ysun-library/src/main.js
+++ ysun-library/src/main.js
@@ -8,9 +8,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
 
-app.mount('#app')
+app.mount('#app');
